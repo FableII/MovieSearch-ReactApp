36244edac7496d385c8745c4e0cae60910b26234
Galikbarov Eugene <52076316+FableII@users.noreply.github.com>
2022-11-05 21:36:54 +0300

    style format in Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css";
 import logo from "../../assets/logo/NetflixLogo.webp";
+import "./Navbar.css";
 
 export const Navbar = () => {
   return (
