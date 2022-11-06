9e68336bf95fb1afd120264a49608f1a3c0c827d
Galikbarov Eugene <52076316+FableII@users.noreply.github.com>
2022-11-06 23:38:54 +0300

    update Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,68 @@
 import { Link } from "react-router-dom";
+import { useAppDispatch, useCurrentUser } from "../../hooks/hooks";
+import { logOut } from "../../redux/slices/userSlice";
 import logo from "../../assets/logo/NetflixLogo.webp";
 import "./Navbar.css";
 
 export const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const { isAuth, username, email } = useCurrentUser() || {};
+
+  const logout = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    dispatch(logOut(email));
+  };
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar__logo">
         <Link to="/">
-          {" "}
-          <img className="app__logo" src={logo} alt="logo" />{" "}
+          <img className="app__logo" src={logo} alt="logo" />
         </Link>
       </div>
+
       <ul className="app__navbar__list">
-        <li className="app__navbar__list-item">
-          <Link to="/search">Search</Link>
-        </li>
-        <li className="app__navbar__list-item">
-          <Link to="/favorites">Favorites</Link>
-        </li>
-        <li className="app__navbar__list-item">
-          <Link to="/history">History</Link>
-        </li>
-        <li className="app__navbar__list-item">
-          <Link to="/signin">Sign in</Link>
-        </li>
-        <li className="app__navbar__list-item">
-          <Link to="/signup">Sign up</Link>
-        </li>
+        {isAuth && (
+          <>
+            <li className="app__navbar__list-item">
+              <Link to="/search">Search</Link>
+            </li>
+            <li className="app__navbar__list-item">
+              <Link to="/favorites">Favorites</Link>
+            </li>
+            <li className="app__navbar__list-item">
+              <Link to="/history">History</Link>
+            </li>
+          </>
+        )}
+
+        {isAuth ? (
+          <>
+            
+            <li onClick={logout} className="app__navbar__list-item logout">
+              Log Out
+            </li>
+            <li className="app__navbar__list-item username">{username?.[0]}</li>
+          </>
+        ) : (
+          <>
+            <li className="app__navbar__list-item">
+              <Link to="/signin">Search</Link>
+            </li>
+            <li className="app__navbar__list-item">
+              <Link to="/signin">Favorites</Link>
+            </li>
+            <li className="app__navbar__list-item">
+              <Link to="/signin">History</Link>
+            </li>
+            <li className="app__navbar__list-item">
+              <Link to="/signin">Sign in</Link>
+            </li>
+            <li className="app__navbar__list-item">
+              <Link to="/signup">Sign up</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
