df62295747a3fccbd785a1bf40ed4e987c5ac390
Galikbarov Eugene <52076316+FableII@users.noreply.github.com>
2022-11-06 16:56:34 +0300

    create middleware

---
+++ src/middleware/checkLoginMiddleware.ts
@@ -0,0 +1,23 @@
+import { RootState } from '../redux/store/store';
+import { Middleware } from "redux";
+
+export const checkLoginMiddleware: Middleware = (store) => (next) => (action) => {
+  let result;
+  const currentStore: RootState = store.getState();
+
+  if (action.type === "user/addUser") {
+    const emailUsed = Object.values(currentStore.user).find(
+      (user) => user.email === action.payload.email
+    );
+
+    if (emailUsed) {
+      alert("Email address already exists! Please, try again using different email.");
+
+      return result;
+    }
+  }
+
+  result = next(action);
+
+  return result;
+};
