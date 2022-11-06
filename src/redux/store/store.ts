3886e128c5a55e53592f85c80e0094482869b302
Galikbarov Eugene <52076316+FableII@users.noreply.github.com>
2022-11-06 16:46:35 +0300

    init: redux-persist | create store

---
+++ src/redux/store/store.ts
@@ -0,0 +1,40 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit"; 
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import userSliceReducer from "../slices/userSlice";
+import { checkLoginMiddleware } from "../../middleware/checkLoginMiddleware";
+
+const rootReducer = combineReducers({
+  user: userSliceReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([checkLoginMiddleware]),
+});
+
+export const persistor = persistStore(store);
+export default store;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
