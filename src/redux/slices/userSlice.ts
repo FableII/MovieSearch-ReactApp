7035bd8c34abd08abaa2fc317d0db9f748ee6008
Galikbarov Eugene <52076316+FableII@users.noreply.github.com>
2022-11-06 15:49:48 +0300

    create userSlice

---
+++ src/redux/slices/userSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface User {
+  [prop: string]: any;
+  username?: string;
+  email?: string;
+  password?: string;
+  isAuth?: boolean;
+  favorites?: { name: string; url: string }[];
+  historySearch?: string[];
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {} as Record<string, User>,
+  reducers: {
+    signIn(state, action) {
+      const currentUser = action.payload;
+      state[currentUser].isAuth = true;
+    },
+
+    logOut(state, action) {
+      const currentUser = action.payload;
+      state[currentUser].isAuth = false;
+    },
+
+    addUser(state, action) {
+      const newUser = action.payload.email;
+      state[newUser] = action.payload;
+    },
+  },
+});
+
+export const { signIn, logOut, addUser } = userSlice.actions;
+
+export default userSlice.reducer;
