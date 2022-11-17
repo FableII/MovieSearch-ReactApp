9627d6354f68a65b00aad733a795465b7b837b6e
Galikbarov Eugene <52076316+FableII@users.noreply.github.com>
2022-11-18 01:17:37 +0300

    bugfix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,20 @@
+import { Suspense } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../../components/Button/Button";
+import { Loader } from "../../components/Loader/Loader";
 import "./Home.css";
 
 export const Home = () => {
   return (
     <div className="app__home">
-      <h1 className="app__home-header">
-        Unlimited movies, TV shows, and more. Watch anywhere. Cancel anytime.
-      </h1>
-      <Link to="/movies">
-        <Button buttonName="Start your journey" />
-      </Link>
+      <Suspense fallback={<Loader />}>
+        <h1 className="app__home-header">
+          Unlimited movies, TV shows, and more. Watch anywhere. Cancel anytime.
+        </h1>
+        <Link to="/movies">
+          <Button buttonName="Start your journey" />
+        </Link>
+      </Suspense>
     </div>
   );
 };
